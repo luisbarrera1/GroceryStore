33b428e079035a71dbb906d5f7f0d33748b1faff
luisbarrera1 <labarrera@mail.csuchico.edu>
2019-10-07 19:29:50 -0700

    created a repeating list using the do while loop

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
+// Author Luis Barrera                                                                                                               
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string item;
+        do
+        { 
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                cin.ignore();
+                if( input == 'A' || input == 'a') 
+                {   
 
-  return 0;
+                        cout<<"What is the item?"<<endl;
+                        cin>> item;
+                        cin.ignore();
+
+                        if(numItems >= 5)
+                        { 
+                                cout<<"You'll need a bigger list!"<<endl;
+
+                        }
+                        numItems++;
+                }
+
+        }
+        while(input != 'Q'&& input != 'q');
+
+        return 0;
 }
